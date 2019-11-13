dc9298990b43666bc672e701ea1616f2f116632e
Maheshron <mahesh28031996@gmail.com>
2019-11-13 21:57:36 +0530

    f

--- first.js
+++ first.js
@@ -1,13 +1,10 @@
 console.log('hello world');
 
-<<<<<<< HEAD
 function target(){
    var a = prompt('how are you'); 
    alert(a);
 
-=======
 
 function fun(){
     alert('you clicked');
->>>>>>> mahesh
 }
